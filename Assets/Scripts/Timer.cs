c3e3fb092f45beebe13486661e3def67cc48eb8d
Elijah Temwani Zulu <modeaxe@Gmail.com>
2021-06-03 12:12:46 -0400

    added timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime;
+    public float startingTime = 10f;
+
+    [SerializeField] private TextMeshPro countdownText;
+
+    private int seconds;
+    private int minutes;
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+    void Update()
+    {
+        currentTime += -1 * Time.deltaTime;
+        minutes = (int) currentTime / 60;
+        seconds = (int)currentTime % 60;
+        countdownText.text = minutes.ToString("00") +":"+ seconds.ToString("00");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
+
